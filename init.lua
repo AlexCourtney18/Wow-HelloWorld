a3af658f805d9d895f84137b3912a86c8189b4b2
Alex <lex.courtney18@gmail.com>
2023-04-05 13:51:05 -0500

    write function to handle slash commands

--- init.lua
+++ init.lua
@@ -1,31 +1,64 @@
-local _, core = ...; --Namespace
+local _, core = ... --Namespace
 
 --------------------------------------
 -- Custom Slash Command
 --------------------------------------
 core.commands = {
     ["config"] = core.Config.Toggle, -- this is a function (no knowledge of Config object)
-    
-        ["help"] = function ()
-            print(" ");
-            core:Print("List of slash commands:")
-            core:Print("|cff00cc66/at config|r - shows config menu");
-            core:Print("|cff00cc66/at help|r - shows help info");
-            print(" ");
-        end,
+    ["help"] = function()
+        print(" ")
+        core:Print("List of slash commands:")
+        core:Print("|cff00cc66/at config|r - shows config menu")
+        core:Print("|cff00cc66/at help|r - shows help info")
+        print(" ")
+    end,
+    ["example"] = {
+        ["test"] = function(...)
+            core:Print("My Value:", tostringall(...))
+        end
+    }
+}
 
-        ["example"] = {
-            ["test"] = function(...)
-                core:Print("My Value:", tostringall(...));
+local function HandleSlashCommands(str)
+    if (#str == 0) then
+        -- User just enetered "/at" with no additional args.
+        core.commands.help()
+        return
+    end
+
+    local args = {}
+    for _, arg in ipairs({string.split(" ", str)}) do
+        if (#arg > 0) then
+            table.insert(args, arg)
+        end
+    end
+
+    local path = core.commands -- required for updating found table
+
+    for id, arg in ipairs(args) do
+        if (#arg > 0) then -- if string length is greater than 0
+            arg = arg:lower()
+            if (path[arg]) then
+                if (type(path[arg]) == "function") then
+                    -- all remaining args passed to our function!
+                    path[arg](select(id + 1, unpack(args)))
+                    return
+                elseif (type(path[arg]) == "table") then
+                    path = parth[arg] -- another sub-table found!
+                end
+            else
+                -- does not exist!
+                core.commands.help()
+                return
             end
-        }
-};
-
+        end
+    end
+end
 
 function core:Print(...)
-    local hex = select(4, self.Config:GetThemeColor());
-    local prefix = string.format("|cff%s%s|r", hex:upper(), "Aura Tracker:");
-    DEFAULT_CHAT_FRAME:AddMessage(string.join(" ", prefix, ...));
+    local hex = select(4, self.Config:GetThemeColor())
+    local prefix = string.format("|cff%s%s|r", hex:upper(), "Aura Tracker:")
+    DEFAULT_CHAT_FRAME:AddMessage(string.join(" ", prefix, ...))
 end
 
 -- WARNING: self automatically becomes events frame!
@@ -47,7 +80,7 @@ function core:init(event, name)
     SLASH_VAULT1 = "/vault"
 
     SLASH_RELOADUI1 = "/rl" -- Quicker reloading
-    SlashCmdList.RELOADUI = ReloadUI;
+    SlashCmdList.RELOADUI = ReloadUI
 
     SLASH_FRAMESTK1 = "/fs" -- For quicker access to frame stack
     SlashCmdList.FRAMESTK = function()
@@ -55,13 +88,12 @@ function core:init(event, name)
         FrameStackTooltip_Toggle()
     end
 
-    SLASH_AuraTracker1 = "/at";
-    SlashCmdList.AuraTacker = HandleSlashCommands;
+    SLASH_AuraTracker1 = "/at"
+    SlashCmdList.AuraTacker = HandleSlashCommands
 
-    core:Print("Welcome back", UnitName("player") .. "!");
+    core:Print("Welcome back", UnitName("player") .. "!")
 end
 
-
-local events = CreateFrame("Frame");
-events:RegisterEvent("ADDON_LOADED");
-events:SetScript("OnEvent", core.init);
+local events = CreateFrame("Frame")
+events:RegisterEvent("ADDON_LOADED")
+events:SetScript("OnEvent", core.init)
