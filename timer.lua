597a9046bd2f7e189d53aeb9907559745798cec3
Alex <lex.courtney18@gmail.com>
2023-04-05 12:31:55 -0500

    add placeholder code to timer.lua

---
+++ timer.lua
@@ -0,0 +1,36 @@
+----------------------
+-- Namespaces
+----------------------
+
+local _, core = ...;
+core.Timer = {};
+
+local Timer = core.Timer;
+
+----------------------
+-- Timer functions
+----------------------
+--[[
+	Should return an instance of a Timer (using metatables).
+	A "static" function.
+--]]
+
+function Timer:Create()
+
+end
+
+--[[
+	Should update the duration of the timer using events.
+--]]
+
+function Timer:Update()
+
+end
+
+--[[
+	Destroy the timer after it has expired (recylce it).
+--]]
+
+function Timer:Destroy()
+
+end
