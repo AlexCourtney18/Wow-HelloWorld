8481473b15072fd7df5ccaba04b91a9ddad60510
Alex <lex.courtney18@gmail.com>
2023-04-06 12:00:56 -0500

    Setclipschildren to true, moved scroll bar to inside of the config frame

--- config.lua
+++ config.lua
@@ -34,7 +34,7 @@ function Config:GetThemeColor()
 end
 
 function Config:CreateButton(point, relativeFrame, relativePoint, yOffset, text)
-    local btn = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate")
+    local btn = CreateFrame("Button", nil, UIConfig.ScrollFrame, "GameMenuButtonTemplate")
     btn:SetPoint(point, relativeFrame, relativePoint, 0, yOffset)
     btn:SetSize(140, 40)
     btn:SetText(text)
@@ -58,17 +58,27 @@ function Config:CreateMenu()
     UIConfig.ScrollFrame:SetPoint("TOPLEFT", VaultFrameConfigDialogBG, "TOPLEFT", 4, -8);
     UIConfig.ScrollFrame:SetPoint("BOTTOMRIGHT", VaultFrameConfigDialogBG, "BOTTOMRIGHT", -3, 4);
 
+    UIConfig.ScrollFrame:SetClipsChildren(true);
+
     local child = CreateFrame("Frame", nil, UIConfig.ScrollFrame);
     child:SetSize(308, 500);
 
+    child.bg = child:CreateTexture(nil, "BACKGROUND");
+    child.bg:SetAllPoints(true);
+    child.bg:SetColorTexture(0.2, 0.6, 0, 0.8);
+
     UIConfig.ScrollFrame:SetScrollChild(child);
+
+    UIConfig.ScrollFrame.ScrollBar:ClearAllPoints();
+    UIConfig.ScrollFrame.ScrollBar:SetPoint("TOPLEFT", UIConfig.ScrollFrame, "TOPRIGHT", -12, -18);
+    UIConfig.ScrollFrame.ScrollBar:SetPoint("BOTTOMRIGHT", UIConfig.ScrollFrame, "BOTTOMRIGHT", -4, 18);
 
     ---------------------------------
     -- Buttons
     ---------------------------------
 
     -- Save Button:
-    UIConfig.saveBtn = self:CreateButton("CENTER", UIConfig, "TOP", -70, "Save")
+    UIConfig.saveBtn = self:CreateButton("CENTER", child, "TOP", -70, "Save")
 
     -- Reset Button:
     UIConfig.resetBtn = self:CreateButton("TOP", UIConfig.saveBtn, "BOTTOM", -10, "Reset");
@@ -81,7 +91,7 @@ function Config:CreateMenu()
     ---------------------------------
 
     -- Slider 1:
-    UIConfig.slider1 = CreateFrame("SLIDER", nil, UIConfig, "OptionsSliderTemplate")
+    UIConfig.slider1 = CreateFrame("SLIDER", nil, UIConfig.ScrollFrame, "OptionsSliderTemplate")
     UIConfig.slider1:SetPoint("TOP", UIConfig.loadBtn, "Bottom", 0, -20)
     UIConfig.slider1:SetMinMaxValues(1, 100)
     UIConfig.slider1:SetValue(50)
@@ -89,7 +99,7 @@ function Config:CreateMenu()
     UIConfig.slider1:SetObeyStepOnDrag(true)
 
     -- Slider 2:
-    UIConfig.slider2 = CreateFrame("SLIDER", nil, UIConfig, "OptionsSliderTemplate")
+    UIConfig.slider2 = CreateFrame("SLIDER", nil, UIConfig.ScrollFrame, "OptionsSliderTemplate")
     UIConfig.slider2:SetPoint("TOP", UIConfig.slider1, "BOTTOM", 0, -20)
     UIConfig.slider2:SetMinMaxValues(1, 100)
     UIConfig.slider2:SetValue(40)
@@ -101,12 +111,12 @@ function Config:CreateMenu()
     ---------------------------------
 
     -- Check Button 1:
-    UIConfig.checkBtn1 = CreateFrame("CheckButton", nil, UIConfig, "UICheckButtonTemplate")
+    UIConfig.checkBtn1 = CreateFrame("CheckButton", nil, UIConfig.ScrollFrame, "UICheckButtonTemplate")
     UIConfig.checkBtn1:SetPoint("TOPLEFT", UIConfig.slider2, "BOTTOMLEFT", -10, -40)
     UIConfig.checkBtn1.text:SetText("My Check Button!")
 
     -- Check Button 2:
-    UIConfig.checkBtn2 = CreateFrame("CheckButton", nil, UIConfig, "UICheckButtonTemplate")
+    UIConfig.checkBtn2 = CreateFrame("CheckButton", nil, UIConfig.ScrollFrame, "UICheckButtonTemplate")
     UIConfig.checkBtn2:SetPoint("TOPLEFT", UIConfig.checkBtn1, "BOTTOMLEFT", 0, -10)
     UIConfig.checkBtn2.text:SetText("Another Check Button!")
     UIConfig.checkBtn2:SetChecked(true)
